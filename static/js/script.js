bb66508106a3d9403e0aa4b801208069ea61cdd0
Upshur06 <mupshur2005@gmail.com>
2020-12-09 00:06:47 -0500

    reset function

--- static/js/script.js
+++ static/js/script.js
@@ -2,14 +2,20 @@
 
 let flexBoxResult = document.getElementById('flex-box-result');
 
-
-let days = 365;
-
 function ageInDays(){
 
     let birthYear = prompt('Please Enter the year you were born');
     let currentYear = prompt('Please Enter the current year');
-    let ageInDays = (currentYear - birthYear) * days;
-    flexBoxResult.innerHTML = `You are ${ageInDays} days old!!!`;
+    let days = 365;
+    let displayAgeInDays = (currentYear - birthYear) * days;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode(`You are ${displayAgeInDays} days old!!!`)
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    flexBoxResult.appendChild(h1);
+
 }
 
+function reset(){
+    flexBoxResult.remove();
+}
